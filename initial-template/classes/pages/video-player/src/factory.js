4858e3da46b1992651e2f0790c405b630203cdf8
Caio Souza Lima <caiosouzalima2002@gmail.com>
2023-03-05 03:41:25 -0300

    fix: video src

--- initial-template/classes/pages/video-player/src/factory.js
+++ initial-template/classes/pages/video-player/src/factory.js
@@ -27,7 +27,7 @@ async function getWorker() {
       const blinked = await service.handBlinked(video)
       if(!blinked) return;
       workerMock.onmessage({ data: { blinked }})
-     },
+    },
     //  vai ser sobreescrito pela controller
     onmessage(msg) { }
   }
@@ -39,16 +39,20 @@ async function getWorker() {
   return workerMock
 }
 
+const view = new View()
+const [rootPath] = window.location.href.split('/pages/')
+view.setVideoSrc(`${rootPath}/assets/video.mp4`)
+
 const worker = await getWorker()
 
 const camera = await Camera.init()
-const [rootPath] = window.location.href.split('/pages/')
+
 const factory = {
   async initialize() {
     return Controller.initialize({
       view: new View(),
       worker,
-      camera
+      camera,
     })
   }
 }
